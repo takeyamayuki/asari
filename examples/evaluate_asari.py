82ab36ba02824db883214517b2090432b9d37607
Yuki Takeyama <namiki.takeyama@gmail.com>
2022-10-24 18:45:23 +0900

    add nega/posi

--- examples/evaluate_asari.py
+++ examples/evaluate_asari.py
@@ -10,7 +10,8 @@ cnt = 0
 
 for str in df_wrime['Sentence']:
     # print(str)
-    print(sonar.ping(str), end='\n')
+    ans = sonar.ping(str)
+    print(ans['top_class'], end='\n')
     cnt += 1
     if cnt == 10:
         break
