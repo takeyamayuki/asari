5631269c853839631d04beeefd5a0932c49c1d7f
Yuki Takeyama <namiki.takeyama@gmail.com>
2022-10-26 15:09:14 +0900

    delete examples

--- examples/test_asari.py
+++
@@ -1,6 +0,0 @@
-from asari.api import Sonar
-sonar = Sonar()
-text = "表情筋が衰えてきてる。まずいな…"
-
-
-print(sonar.ping(text=text))
--- examples/test_asari_copy.py
+++
@@ -1,4 +0,0 @@
-from asari.api import Sonar
-sonar = Sonar()
-
-print(sonar.ping(text="広告多すぎる♡"))
