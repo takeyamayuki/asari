cd9b3a412c9d3a4a295cbe95c86e127642f9650f
takeyamayuki <namiki.takeyama@gmail.com>
2023-01-11 18:26:27 +0900

    add print

--- asari/api.py
+++ asari/api.py
@@ -7,14 +7,31 @@ from asari.preprocess import tokenize
 
 class Sonar:
     def __init__(self):
-        pipeline_file = pathlib.Path(__file__).parent / "data" / "pipeline.onnx"
+        pipeline_file = pathlib.Path(
+            __file__).parent / "data" / "pipeline.onnx"
         self.sess = rt.InferenceSession(str(pipeline_file))
         self.input_name = self.sess.get_inputs()[0].name
         self.prob_name = self.sess.get_outputs()[1].name
 
+        input_name = self.sess.get_inputs()[0].name
+        print("Input name  :", input_name)
+        input_shape = self.sess.get_inputs()[0].shape
+        print("Input shape :", input_shape)
+        input_type = self.sess.get_inputs()[0].type
+        print("Input type  :", input_type)
+
+        output_name = self.sess.get_outputs()[0].name
+        print("Output name  :", output_name)
+        output_shape = self.sess.get_outputs()[0].shape
+        print("Output shape :", output_shape)
+        output_type = self.sess.get_outputs()[0].type
+        print("Output type  :", output_type)
+
     def ping(self, text: str):
         tokenized = tokenize(text)
-        proba = self.sess.run([self.prob_name], {self.input_name: [tokenized]})[0][0]
+        proba = self.sess.run(
+            [self.prob_name], {self.input_name: [tokenized]})[0][0]
+        print(tokenized, proba)
         res = {
             "text": text,
             "top_class": max(proba, key=lambda k: proba[k]),
@@ -23,3 +40,9 @@ class Sonar:
             ],
         }
         return res
+
+
+if __name__ == "__main__":
+    sonar = Sonar()
+    # print(sonar.input_name, sonar.prob_name)
+    print(sonar.ping("今日はいい天気ですね"))
